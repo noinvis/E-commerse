77eeacee3129c59e0db02e4b70e4b37bb73aded0
noinvis <lazizbek.sn@gmail.com>
2025-09-11 13:50:19 +0500

    api

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -3,3 +3,11 @@ import axios from "axios";
 export const api = axios.create({
     baseURL: "https://api.errorchi.uz/"
 })
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token")
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
